8de464e1299558deff1e2d4a326da1865e28fe7d
alekmak <alex1snow@yandex.ru>
2021-06-08 23:21:40 +0300

    Initial commit

--- src/main/java/com/example/webTgm/WebTgmApplication.java
+++ src/main/java/com/example/webTgm/WebTgmApplication.java
@@ -3,11 +3,12 @@ package com.example.webTgm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class WebTgmApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebTgmApplication.class, args);
+		System.out.println("Hello world");
+		//SpringApplication.run(WebTgmApplication.class, args);
 	}
 
 }
